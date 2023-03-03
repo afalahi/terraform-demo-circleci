57935f8c42f74a479553bcbec87343aa8ee3ce00
Ali Falahi <ali@afalahi.com>
2023-03-03 09:58:17 -0700

    update config.yml

--- aws/s3_bucket_sse_disabled.tf
+++ aws/s3_bucket_sse_disabled.tf
@@ -18,10 +18,10 @@ resource "aws_s3_bucket" "mybucket1" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "example2" {
   bucket = aws_s3_bucket.mybucket1.bucket
 
-  rule {
-    apply_server_side_encryption_by_default  {
-        kms_master_key_id = "some-key"
-        sse_algorithm     = "AES256"
-    }
-  }
+  # rule {
+  #   apply_server_side_encryption_by_default  {
+  #       kms_master_key_id = "some-key"
+  #       sse_algorithm     = "AES256"
+  #   }
+  # }
 }
